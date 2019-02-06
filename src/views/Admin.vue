a7f8e37e822e458571903d4f2b08251ff7b3cb29
Andre Dargains <andre.dargains@gmail.com>
2019-02-06 23:44:45 +0000

    ADD: success page;

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,6 +1,13 @@
 <template>
 <section class="admin">
   <Bouncer v-if="!logged" />
+  <article class="admin__success" v-else-if="showSuccess">
+    <h2>Fruta submetida!</h2>
+    <p>As notificações já foram enviadas!</p>
+    <figure @click="$router.go('/')">
+      <img src="../assets/positive.svg" alt="">
+    </figure>
+  </article>
   <div class="admin__main" v-else>
     <ul class="admin__list">
       <li class="admin__item" v-for="fruit in fruits" :key="fruit.name">
@@ -34,7 +41,8 @@ export default {
 	},
 	data() {
 		return {
-			list: []
+			list: [],
+      showSuccess: false
 		}
 	},
 	computed: {
@@ -49,7 +57,8 @@ export default {
 		submit() {
 			this.$store.dispatch('submitFruit', this.list).then(() => {
 				console.log('Fruta do Dia submetida!');
-				this.$router.push('/');
+        this.showSuccess = true;
+				// this.$router.push('/');
 			});
 		},
 		toggleFruit(event) {
@@ -66,108 +75,141 @@ export default {
 
 <style lang="scss">
 .admin {
-    height: auto;
-    max-width: 800px;
-    margin: 0 auto;
-    &__list {
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        flex-direction: column;
-        padding-bottom: 80px;
-        li + li {
-            border-top: 1px solid lightgrey;
-        }
+  height: auto;
+  max-width: 800px;
+  margin: 0 auto;
+  &__list {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+    padding-bottom: 80px;
+    li + li {
+      border-top: 1px solid lightgrey;
     }
-    &__item {
-        width: 100%;
-        padding: 0 20px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        height: 80px;
-        i {
-            height: 60px;
-            width: 50px;
-            margin-right: 20px;
-            display: inline-flex;
-            vertical-align: middle;
-            align-items: center;
-            justify-content: center;
-        }
-        p {
-            font-family: Quicksand;
-            font-size: 16px;
-            display: inline-block;
-            vertical-align: middle;
-        }
+  }
+  &__item {
+    width: 100%;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 80px;
+    i {
+      height: 60px;
+      width: 50px;
+      margin-right: 20px;
+      display: inline-flex;
+      vertical-align: middle;
+      align-items: center;
+      justify-content: center;
     }
-    &__buttons {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        button {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 80px;
-            color: white;
-            border: 0;
-            padding: 0;
-            margin: 0;
-            -webkit-appearance: none;
-            cursor: pointer;
-            img {
-                max-width: 40px;
-            }
-            &.back {
-                background-color: var(--red);
-                flex: 1;
-            }
-            &.submit {
-                background-color: var(--green);
-                flex: 2;
-            }
-        }
+    p {
+      font-family: Quicksand;
+      font-size: 16px;
+      display: inline-block;
+      vertical-align: middle;
     }
+  }
+  &__buttons {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 80px;
+      color: white;
+      border: 0;
+      padding: 0;
+      margin: 0;
+      -webkit-appearance: none;
+      cursor: pointer;
+      img {
+        max-width: 40px;
+      }
+      &.back {
+        background-color: var(--red);
+        flex: 1;
+      }
+      &.submit {
+        background-color: var(--green);
+        flex: 2;
+      }
+    }
+  }
+  &__success {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    z-index: 1;
+    height: 100vh;
+    width: 100%;
+    position: absolute;
+    background-color: var(--green);
+    top: 0;
+    left: 0;
+    padding: 30px;
+    color: var(--white);
+    h2 {
+      font-size: 60px;
+      line-height: 0.8;
+      margin: 0 0 60px;
+      letter-spacing: 4.5px;
+    }
+    p {
+      font-size: 16px;
+      font-family: 'Quicksand', sans-serif;
+      font-weight: 700;
+      letter-spacing: 1px;
+      line-height: 1.2;
+    }
+    figure {
+      margin: 40px auto;
+      max-width: 50px;
+      cursor: pointer;
+    }
+  }
 }
 .toggle {
-    label {
-        width: 60px;
-        height: 32px;
-        border-radius: 16px;
-        background-color: var(--gray);
-        display: inline-block;
-        vertical-align: middle;
-        margin-left: 5px;
-        transition: background-color 0.2s ease-in;
-        cursor: pointer;
-        position: relative;
-        &:after {
-            content: '';
-            display: block;
-            position: absolute;
-            top: 2px;
-            left: 2px;
-            width: 28px;
-            height: 28px;
-            border-radius: 50%;
-            background-color: white;
-            box-shadow: 0 2px 4px 0 rgba(gray, 0.35);
-            transition: left 0.1s ease-in;
-        }
+  label {
+    width: 60px;
+    height: 32px;
+    border-radius: 16px;
+    background-color: var(--gray);
+    display: inline-block;
+    vertical-align: middle;
+    margin-left: 5px;
+    transition: background-color 0.2s ease-in;
+    cursor: pointer;
+    position: relative;
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      width: 28px;
+      height: 28px;
+      border-radius: 50%;
+      background-color: white;
+      box-shadow: 0 2px 4px 0 rgba(gray, 0.35);
+      transition: left 0.1s ease-in;
     }
-    input:checked ~ label {
-        background-color: var(--green);
-        &:after {
-            left: 30px;
-        }
+  }
+  input:checked ~ label {
+    background-color: var(--green);
+    &:after {
+      left: 30px;
     }
-    input {
-        display: none;
-    }
+  }
+  input {
+    display: none;
+  }
 }
 </style>
